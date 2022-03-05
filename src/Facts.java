0372eaab719a870a350b9369a916564c7d0a947e
Yevhen <darovykhy@gmail.com>
2022-03-05 14:36:38 +0200

    1 commit

---
+++ src/Facts.java
@@ -0,0 +1,25 @@
+public class Facts {
+
+    public static void main(String [] args){
+
+        byte legsQuantity = 2;
+        short height = 176;
+        int headHairQuantity = 100000;
+        long starsQuantity = 250000000000L;
+        float weight = 68.5f;
+        double bottomAreaOfMyCup = Math.PI*5*5;
+        char lovelyCharacter = (char) 128526;
+        boolean factoConfirmer = true;
+
+        System.out.println("I have " + legsQuantity + " legs");
+        System.out.println("My height is: " + height + " sm");
+        System.out.println("I have about " + headHairQuantity + " hairs on my head");
+        System.out.println("I live in Galaxy that has about " + starsQuantity + " stars");
+        System.out.println("My weight is: " + weight + " kg");
+        System.out.println("Bottom area of my lovely cup is: " + bottomAreaOfMyCup + " sm");
+        System.out.println("My lovely character is: " + lovelyCharacter);
+        System.out.println("My name is Yevhen - " + factoConfirmer);
+
+    }
+
+}
